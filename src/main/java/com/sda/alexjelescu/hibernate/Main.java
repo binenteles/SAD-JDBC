62f5e12aac50359e8050044e86ab1fe9d580b3a9
alex <alexjelescu@gmail.com>
2021-05-11 21:30:18 +0300

    create main class

--- src/main/java/com/sda/alexjelescu/hibernate/Main.java
+++ src/main/java/com/sda/alexjelescu/hibernate/Main.java
@@ -1,2 +1,38 @@
-package com.sda.alexjelescu.hibernate;public class Main {
+package com.sda.alexjelescu.hibernate;
+
+import com.sda.alexjelescu.hibernate.model.Department;
+import com.sda.alexjelescu.hibernate.repository.DepartmentRepository;
+import com.sda.alexjelescu.hibernate.utils.SessionManager;
+
+public class Main {
+    public static void main(String[] args) {
+        DepartmentRepository departmentRepository = new DepartmentRepository();
+        departmentRepository.save("HR");
+
+        Department department = departmentRepository.findById(1);
+        System.out.println(department);
+
+        department.setName("HR_Department");
+        departmentRepository.update(department);
+
+        department = departmentRepository.findById(1);
+        System.out.println(department);
+
+        departmentRepository.delete(department);
+
+        department = departmentRepository.findById(1);
+        System.out.println(department);
+
+        departmentRepository.save("IT");
+
+        Department department2 = departmentRepository.findById(2);
+        System.out.println(department2);
+
+        SessionManager.shutDown();
+
+
+
+
+
+    }
 }
